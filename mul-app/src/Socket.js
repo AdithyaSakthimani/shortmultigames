bafb8b5c578779bda4f6dad9e20c8496f1ec7881
AdithyaSakthimani <adithyasakthimani@gmail.com>
2025-02-24 14:40:20 +0530

    changed server address

--- mul-app/src/Socket.js
+++ mul-app/src/Socket.js
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
-// const SOCKET_URL = "https://shortmultigames.onrender.com";
-const SOCKET_URL = "http://localhost:5000";
-
+ const SOCKET_URL = "https://shortmultigames.onrender.com";
 const socket = io(SOCKET_URL, {
   autoConnect: true,
   reconnection: true,
